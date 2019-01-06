61db82bb65ca48585201f2da83ab544d138e61df
Taylor Williams <taylor@thewilliams.net>
2019-01-06 23:05:47 +0000

    Automatically backed up by Learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,7 +1,13 @@
 class SessionsController < ApplicationController
 
   def create
-
+    u_hash = request.env['omniauth.auth']
+    if user = User.find_by(email: u_hash['info']['email'])
+      @user = user
+    else
+      @user = User.new(email: u_hash['info']['email'], name: u_hash['info']['name'])
+    end
+    redirect_to root_path
   end
 
 end
