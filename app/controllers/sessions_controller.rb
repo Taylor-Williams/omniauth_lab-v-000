6e99cc5e58c2694fa425390c4ca74ca830c9b02a
Taylor Williams <taylor@thewilliams.net>
2019-01-06 23:29:23 +0000

    Automatically backed up by Learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,7 +2,7 @@ class SessionsController < ApplicationController
 
   def create
     @auth = request.env['omniauth.auth']
-    binding.pty
+    binding.pry
     if user = User.find_by(email: @auth['info']['email'])
       @user = user
     else
