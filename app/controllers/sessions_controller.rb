ded2d8bc7878ff25386b34d6c6521f0a712252a9
Taylor Williams <taylor@thewilliams.net>
2019-01-06 23:31:22 +0000

    Automatically backed up by Learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,7 +2,7 @@ class SessionsController < ApplicationController
 
   def create
     @auth = request.env['omniauth.auth']
-    if user = User.find_by(email: @auth['uid'])
+    if user = User.find_by(uid: @auth['uid'])
       @user = user
     else
       @user = User.create(email: @auth['info']['email'], name: @auth['info']['name'], uid: @auth['uid'])
